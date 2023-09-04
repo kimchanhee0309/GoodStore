5e3a79ecb4e42aa36a3bed1d65b37cc77f2e2b68
WgoodK <jowkkk3@naver.com>
2023-09-05 08:57:42 +0900

    Updated spin.cs

--- Assets/Scripts/spin.cs
+++ Assets/Scripts/spin.cs
@@ -7,7 +7,7 @@ public class spin : MonoBehaviour
     public float rotationSpeed = 0.1f;
     public float brrcnt = 0;
     public float level = 30;
-    // Start is called before the first frame update
+
     void Start()
     {
 
