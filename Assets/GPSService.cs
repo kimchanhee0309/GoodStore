bd6137925ef678ad050e017fa5ea287444295506
Chanhee <chanhee27@naver.com>
2023-09-06 21:28:40 +0900

    add
    adddddd

--- Assets/GPSService.cs
+++ Assets/GPSService.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Android;
+using Google.Maps;
+using Google.Maps.Coord;
+
 //using System.Numerics;
 //using UnityEngine.UIElements;
 
 public class GPSService : MonoBehaviour
 {
     public Text textMsg;
+    public GameObject markerPrefab; // 마커 프리팹
+
+    private Vector2 lastGPSPosition;
+    private GameObject currentMarker;
 
     IEnumerator Start()
     {
@@ -62,10 +69,33 @@ public class GPSService : MonoBehaviour
                     + Input.location.lastData.longitude + " "
                     + Input.location.lastData.horizontalAccuracy;
 
+                // 위치가 변경되었을 때 마커를 업데이트합니다.
+                UpdateMarkerPosition(new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude));
+
                 yield return new WaitForSeconds(1);
             }
         }
         //Input.location.Stop();
+    }
+
+    // 위치 정보를 업데이트하고 마커를 이동합니다.
+    private void UpdateMarkerPosition(Vector2 newPosition)
+    {
+        if (currentMarker == null)
+        {
+            // 처음 위치 정보를 받으면 마커를 생성합니다.
+            currentMarker = Instantiate(markerPrefab);
+        }
+
+        // 현재 위치와 이전 위치를 비교하여 위치가 변경되었을 때만 마커를 이동시킵니다.
+        if (newPosition != lastGPSPosition)
+        {
+            // Unity의 좌표계로 변환하여 마커의 위치를 업데이트합니다.
+            currentMarker.transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+
+            // 이전 위치를 현재 위치로 업데이트합니다.
+            lastGPSPosition = newPosition;
+        }
     }
 
     public Vector2 GetGPSInfo()
