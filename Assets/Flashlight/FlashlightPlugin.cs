cc783988d504256007c73d45770ef3cab449ddf8
irungjangs <irungjangs@gmail.com>
2023-09-06 11:31:01 +0900

    Add Flashlight feature
    Scene : scan
    Plugin : flashlight library
    Script : flashlight

---
+++ Assets/Flashlight/FlashlightPlugin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightPlugin : MonoBehaviour
+{
+    public AndroidJavaClass javaObject;
+    bool isFlashOn = false;
+    void Start()
+    {
+        javaObject = new AndroidJavaClass("com.myflashlight.flashlightlib.Flashlight");
+    }
+
+    public void TurnOn()
+    {
+        javaObject.CallStatic("on", GetUnityActivity());
+        isFlashOn = true;
+    }
+    
+    public void TurnOff()
+    {
+        javaObject.CallStatic("off", GetUnityActivity());
+        isFlashOn = false;
+    }
+
+    public void Tougletriggered()
+    {
+        if (isFlashOn)
+        {
+            TurnOff();
+        }
+        else
+        {
+            TurnOn();
+        }
+    }
+
+     AndroidJavaObject GetUnityActivity(){
+         using ( var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+         {
+             return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+         }
+     }
+
+        
+        
+}
