61d8e12dc9a54f056347b65f1866e3d66a75f2e7
irungjangs <irungjangs@gmail.com>
2023-09-06 13:27:07 +0900

    Update BackButtonHandler.cs

--- Assets/Scripts/BackButtonHandler.cs
+++ Assets/Scripts/BackButtonHandler.cs
@@ -14,17 +14,20 @@ public class BackButtonHandler : MonoBehaviour
         //폰에서 뒤로가기 키를 누를 때
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            //후레시가 씬에 있다면
             if(flashlightScript != null)
             {
+                //후레시 끄기
                 flashlightScript.TurnOff();
             }
 
             //홈 씬이라면
             if (SceneManager.GetActiveScene().name == "Home")
             {
-                //앱을 끄기 전에 폰 후레시 끄기
-                if (flashlightScript != null && !flashlightScript.isFlashOn)
+                //앱을 끄기 전에 후레시 켜져있는지 확인해서 켜져있다면
+                if (flashlightScript != null && flashlightScript.isFlashOn)
                 {
+                    //폰 후레시 끄기
                     flashlightScript.TurnOff();
                 }
             }
